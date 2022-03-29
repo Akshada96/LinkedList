5c2cfe26173c3e023a40deaf783713348cb0a638
Akshada <akshumore96@gmail.com>
2022-03-29 19:46:24 +0530

    Added insert after method

--- src/com/bridgelabz/MainClass.java
+++ src/com/bridgelabz/MainClass.java
@@ -6,11 +6,7 @@ public class MainClass {
         list.add(56);
         list.add(30);
         list.add(70);
+        list.insertAfter(30,40);
         list.display();
-        Node searchedNode = list.search(30);
-        if (searchedNode == null)
-            System.out.println("\nData Not Found");
-        else
-            System.out.println("\nData Found");
     }
 }
